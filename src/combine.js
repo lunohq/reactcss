9bf0849abd970be71bbddd2aa69fc0540a761285
Michael Hahn <mwhahn@gmail.com>
2015-11-25 18:54:19 -0800

    Don't use a file called "merge"
    this conflicts with the "merge" library that is used and is also not
    necessary.

--- src/combine.js
+++ src/combine.js
@@ -1,9 +1,25 @@
 'use strict';
 
-const merge = require('./merge');
+const merge = require('merge');
 const mixins = require('./transform-mixins');
+const _ = require('lodash');
+
+function _merge(thingsToBeMerged) {
+  // If its an object, lets just return it
+  if (_.isObject(thingsToBeMerged) && !_.isArray(thingsToBeMerged)) {
+    return thingsToBeMerged;
+  }
+
+  // If the array only has one object in it, return it
+  if (thingsToBeMerged.length === 1) {
+    return thingsToBeMerged[0];
+  }
+
+  // Else, lets just use the merge.js function:
+  return merge.recursive.apply(this, thingsToBeMerged);
+}
 
 module.exports = (styles, customMixins) => {
-  const merged = merge(styles);
+  const merged = _merge(styles);
   return mixins(merged, customMixins);
 };
